128069e2c3b80d69d6a19cebd65ee147ec7aba26
guerra_ms <miguelguerra4@gmail.com>
2020-11-26 15:34:09 +0000

    Departments - Delete and Get

--- Routes/api/departments.js
+++ Routes/api/departments.js
@@ -17,6 +17,19 @@ router.get('/', async (req, res) => {
     }
 })
 
+//routes for GET a particular departments
+router.get('/:id', async (req, res) => {
+    try {
+        const departments = await Departments.findById(req.params.id);
+        if(!departments) {
+            throw Error('No department found');
+        }
+        res.status(200).json(departments);
+    } catch (error) {
+        res.status(400).json({ msg: error })
+    }
+})
+
 //routes for POST department
 router.post('/', async (req, res) => {
     const newDepartment = new Departments(req.body);
@@ -31,4 +44,29 @@ router.post('/', async (req, res) => {
     }
 })
 
+//routes for DELETE a particular department
+router.delete('/:id', async (req, res) => {
+    try {
+        const departments = await Departments.findByIdAndDelete(req.params.id);
+        if(!departments) {
+            throw Error('No department found');
+        }
+        res.status(200).json(departments);
+    } catch (error) {
+        res.status(400).json({ msg: error })
+    }
+})
+
+//routes for UPDATE a particular department
+router.patch('/:id', async (req, res) => {
+    try {
+        const departments = await Departments.findByIdAndUpdate(req.params.id, req.body);
+        if(!departments) {
+            throw Error('No department found');
+        }
+        res.status(200).json(departments);
+    } catch (error) {
+        res.status(400).json({ msg: error })
+    }
+})
 module.exports = router;
